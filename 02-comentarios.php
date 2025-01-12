4f40279a0d6eefcc21619ec005a76d30118b2e68
Rayssa Vicente <rayssavicenteviegas@gmail.com>
2025-01-12 11:08:37 -0300

    colocando o número das aulas

---
+++ 02-comentarios.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <?php
+
+        //Nesse aquivo estou testando um comentário de uma linha
+        # Estou testando o comentário de uma linhas estilo shell
+        echo 'Comando echo<br>';
+
+        print 'Comando print<hr>';
+
+        /*
+        Esse é um comentario de multiplas linhas
+        Estou aqui só  testando
+    
+        */
+    ?>
+    <br>
+    
+   
+    
+    
+</body>
+</html>
