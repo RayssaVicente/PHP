c88783a7138403b5a2ae5a8f1ee12be1a3600cc5
Rayssa Vicente <rayssavicenteviegas@gmail.com>
2025-01-13 09:52:46 -0300

    Aprendendo if e else

---
+++ 08-if-else.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <?php
+    
+    if(2==='2'){
+        echo 'Verdadeiro, <br>';
+    }else{
+        echo 'Falso, <br>';
+    }
+
+    if(2==2){
+        echo 'Verdadeiro, <br>';
+    }else{
+        echo 'Falso,<br>';
+    }
+
+    if(5 !=  2){
+        echo 'Verdadeiro, <br>';
+    }else{
+        echo 'Falso,<br>';
+    }
+
+    if(5 !== '5'){
+        echo 'Verdadeiro, <br>';
+    }else{
+        echo 'Falso,<br>';
+    }
+
+    if(5 > 6){
+        echo 'Verdadeiro, <br>';
+    }else{
+        echo 'Falso,<br>';
+    }
+
+    if(5 < 6){
+        echo 'Verdadeiro, <br>';
+    }else{
+        echo 'Falso,<br>';
+    }
+
+    if(5 <= 15){
+        echo 'Verdadeiro, <br>';
+    }else{
+        echo 'Falso,<br>';
+    }
+    if(5 >= 15){
+        echo 'Verdadeiro, <br>';
+    }else{
+        echo 'Falso,<br>';
+    }
+
+    ?>
+
+
+    <br>
+
+    
+    
+   
+    
+    
+</body>
+</html>
