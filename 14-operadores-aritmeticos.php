c4499fcd025a725aa4140ccc85ba59960e4f5af0
Rayssa Vicente <rayssavicenteviegas@gmail.com>
2025-01-14 11:42:58 -0300

    Conhecendo formas de usar os operadores aritméticos

---
+++ 14-operadores-aritmeticos.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso PHP</title>
+</head>
+<body>
+    
+    <?php
+    $num1 = 13;
+    $num2 = 4;
+    echo "A soma entre $num1 e $num2 é: ". ($num1 + $num2) ;
+    echo "<br/>";
+    echo "A subtração entre $num1 e $num2 é: ". ($num1 - $num2);
+    echo "<br/>";
+    echo "A Multiplicação entre $num1 e $num2 é: ". ($num1 * $num2);
+    echo "<br/>";
+    echo "A Divisão entre $num1 e $num2 é: ". ($num1 / $num2);
+    echo "<br/>";
+    echo "O Modulo entre $num1 e $num2 é: ". ($num1 - $num2);
+   
+    
+    ?>
+    
+</body>
+</html>
