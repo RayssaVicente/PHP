6ebaeee08223dfa8631705525d87eb23bea9cb0f
Rayssa Vicente <rayssavicenteviegas@gmail.com>
2025-01-18 20:19:22 -0300

    Aprendendo a usar o do-while

---
+++ 26-do-while.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <?php
+    
+        $x = 1;
+
+        do {
+    
+            echo 'Entrou no DO WHILE'. '<br>';
+            $x ++;
+        }while($x < 9 );
+            echo 'Entrou no while <br>';
+            echo 'Abobora'
+        
+
+            
+
+            
+
+        
+     ?>
+    
+    
+</body>
+</html>
